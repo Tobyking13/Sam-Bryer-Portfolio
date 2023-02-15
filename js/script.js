d151e15f5ea2b7596de72f7a3ccec2ef2c713af6
Toby King <tobyking13@gmail.com>
2023-02-15 13:32:57 +0000

    updates portfolio/contact wording

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
     });
 });
 
+// const slider = document.querySelector('#portfolio');
+// let position = 0;
+
+// setInterval(() => {
+//   position = (position + 100) % 400;
+//   slider.style.transform = `translateX(-${position}%)`;
+// }, 1000);
